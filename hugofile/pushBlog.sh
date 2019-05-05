2db23876828c0a3af8b848c709d0fd183bdf5531
zhou.chen <chengsanban@outlook.com>
2019-05-05 23:31:30 +0800

    new file

--- hugofile/pushBlog.sh
+++ hugofile/pushBlog.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
-
-cd ./hugo/blog
-echo "进入blog目录....."
-rm -rf public/*
+echo on
+echo "进入静态页面目录....."
+cd /Users/chengsanban/github/chengsanban.github.io
+echo "删除静态网站文件"
+rm 404.html
+rm -rf dist
+rm index.html
+rm -rf post
+rm -rf tags
+rm -rf about
+rm favicon.ico
+rm index.xml
+rm robots.txt
+rm -rf categories
+rm -rf lib
+rm sitemap.xml
+rm -rf content
+rm -rf page
+rm sitemap.xsl
+echo "进入hugo网站目录"
+cd hugofile/blog
 echo "删除public目录文件....."
+rm -rf public/*
+echo "生成新网站静态文件....."
 hugo
-echo "生成新文件....."
-cd ../../chengsanban.github.io
 echo "进入git博客目录....."
-rm -rf ./*
-echo "删除git博客内容....."
-cp -r ../hugo/blog/public/* .
+cd ../..
 echo "拷贝public内容到当前目录....."
-cp -r ../hugo/blog/content ./
-echo "拷贝原始文章....."
-cp -r ../image ./
-echo "拷贝原始图片....."
+cp -r hugofile/blog/public/* .
+echo "删除生成的原始文件"
+rm -rf hugofile/blog/public/*
 echo "开始提交....."
 git add .
 git commit -am "new article"
